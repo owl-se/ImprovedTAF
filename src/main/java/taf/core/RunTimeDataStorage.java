00431b5aade60a1ed343a9991b402249dbb54a21
owl-se <alloc89@mail.ru>
2019-07-31 16:33:09 +0400

    RunTime

---
+++ src/main/java/taf/core/RunTimeDataStorage.java
@@ -0,0 +1,21 @@
+package taf.core;
+
+public class RunTimeDataStorage {
+
+    public static class Statistic {
+
+        private static int caseOrderNumber;
+
+        public static void resetCaseOrderNumber() {
+            caseOrderNumber = 0;
+        }
+
+        public static void incrementCaseOrderNumber() {
+            caseOrderNumber++ ;
+        }
+
+        public static int getCaseOrderNubmer() {
+            return  caseOrderNumber;
+        }
+    }
+}
